89deb137f95efb3085d430a5367588a8111414d8
BrandonSarmany <45214313+BrandonSarmany@users.noreply.github.com>
2018-11-29 04:24:57 -0500

    Add files via upload

--- Insight/Insight/Form1.Designer.cs
+++ Insight/Insight/Form1.Designer.cs
@@ -28,47 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // chart1
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(40, 28);
+            chartArea6.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea6);
+            legend6.Enabled = false;
+            legend6.Name = "Legend1";
+            this.chart1.Legends.Add(legend6);
+            this.chart1.Location = new System.Drawing.Point(35, 56);
             this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.IsVisibleInLegend = false;
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chart1.Series.Add(series1);
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series6.IsVisibleInLegend = false;
+            series6.Legend = "Legend1";
+            series6.MarkerColor = System.Drawing.Color.Blue;
+            series6.MarkerSize = 10;
+            series6.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            series6.Name = "Series1";
+            this.chart1.Series.Add(series6);
             this.chart1.Size = new System.Drawing.Size(893, 519);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
-            this.chart1.UseWaitCursor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(246, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(246, 30);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(136, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(136, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(377, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Submit Coordinates";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(971, 575);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.chart1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Insight/Insight/Form1.cs
+++ Insight/Insight/Form1.cs
@@ -39,7 +39,11 @@ namespace Insight
             chart.AxisX.Interval = 50;
             chart.AxisY.Interval = 50;
 
-            chart1.Series[0].IsVisibleInLegend = false;
+            label1.Text = "Enter X Coordiante";
+            label2.Text = "Enter Y Coordiante";
+           
+
+            
 
 
 
@@ -64,18 +68,25 @@ namespace Insight
             supplyPoints.Add(new Point(325, 345));
             supplyPoints.Add(new Point(450, 50));
             supplyPoints.Add(new Point(460, 410));
+            chart1.Series["Series1"].MarkerSize = 4;
+            chart1.Series["Series1"].MarkerColor = Color.Blue;
+            chart1.Series["Series1"].Color = Color.Blue;
 
 
 
 
-
+            double x1;
+            double y1;
+            double x2;
+            double y2;
             for (int j = 0; j < numberOfSupplyPoints; j++)
             {
 
                 chart1.Series.Add(supplyPointNames[j]);
                 Console.WriteLine(supplyPointNames[j]);
                 chart1.Series[supplyPointNames[j]].ChartType = SeriesChartType.Line;
-                
+                chart1.Series["Series1"].Color = Color.Red;
+
 
                 for (int i = 0; i < customerpointCount; i++)
                 {
@@ -87,23 +98,81 @@ namespace Insight
                 {
                     lines.Add(i);
                 }
-                Console.WriteLine(supplyPoints[j].X);
+               
 
 
                 foreach (int ln in lines)
                 {
+                    
                     chart1.Series[supplyPointNames[j]].Points.AddXY(supplyPoints[j].X, supplyPoints[j].Y);
                     chart1.Series[supplyPointNames[j]].Points.AddXY(points[ln].X, points[ln].Y);
-                    Console.WriteLine("Connected Point:" + points[ln]);
-                    //chart1.Series[supplyPointNames[j]].Color = Color.Transparent;
+                    Console.WriteLine("Connected Point: " + points[ln]);
+                    Console.WriteLine("Supply Points X " + supplyPoints[j].X + " Supply Points Y " + supplyPoints[j].Y);
                     chart1.Series[supplyPointNames[j]].Color = supplyPointColours[j];
-             
+
+
+                     x1 = points[ln].X;
+                     y1 = points[ln].Y;
+                     x2 = supplyPoints[j].X;
+                     y2 = supplyPoints[j].Y;
+   
+                    // I am stuck after this point. This is only calculating the distance from the specific supply point that its attached to,
+                    // I am not sure how to get the distance from that point and all the other supply points to compare them, possibly another 
+                    // for loop which will have the above code in it.
+                    var distance = DistanceCalc(x1, y1, x2, y2);
+                    Console.WriteLine("Distance = " + distance);
+
+                   
+
                 }
 
                 points = new List<PointF>();  
                 lines = new List<int>();
-
             }
+            
         }
+        /*This is the function that i believe works in order to get the distance from one point to its supply point */
+        private static double DistanceCalc(double x1, double y1, double x2, double y2)
+        {
+            var temp = Math.Pow((x2 - x1), 2);
+            var temp2 = Math.Pow((y2 - y1), 2);
+            var distance = Math.Sqrt(temp + temp2);
+
+            return distance;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        /*I got the button to take the values from the text box and turn it into a point on the graph, 
+         * but I was unable to figure out how to implement this into the main method to draw a line to a supply point */ 
+        public void button1_Click(object sender, EventArgs e)
+        {
+          
+            double x;
+            double y;
+
+            bool xval = double.TryParse(textBox1.Text, out x);
+            bool yval = double.TryParse(textBox2.Text, out y);
+
+            if (xval && yval)
+                chart1.Series["Series1"].Points.AddXY(x, y);
+            else 
+
+                MessageBox.Show("Input not accepted, Please Enter a number");
+        }
+       
     }
 }
